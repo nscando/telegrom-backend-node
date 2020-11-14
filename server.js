66ea83bd4727ae7e44a2aad6d82aa4c7eb2a9bfa
nscando <nicolas.scandizzo@gmail.com>
2020-11-14 11:21:48 -0300

    initial commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+
+var app = express();
+
+app.use('/', function (req, res) {
+     res.send('hola')
+});
+
+app.listen(3000);
+console.log('la app esta escuchando en http://localhost:3000');
