68aee90ce2f4c04df05fdc4844c20d5915361a29
nscando <nicolas.scandizzo@gmail.com>
2020-11-15 13:01:31 -0300

    DB configuration

--- components/message/controller.js
+++ components/message/controller.js
@@ -1,6 +1,4 @@
-const { error } = require("../../network/response");
-
-
+const store = require('./store');
 
 function addMessage(user, message) {
      return new Promise((resolve, reject) => {
@@ -13,12 +11,19 @@ function addMessage(user, message) {
                message: message,
                date: new Date(),
           };
-          console.log(fullMessage);
+          store.add(fullMessage);
           resolve(fullMessage);
      });
 
 };
 
+function getMessages() {
+     return new Promise((resolve, reject) => {
+          resolve(store.list());
+     })
+}
+
 module.exports = {
-     addMessage
+     addMessage,
+     getMessages
 };
--- components/message/network.js
+++ components/message/network.js
@@ -5,12 +5,13 @@ const router = express.Router();
 
 
 router.get('/', function (req, res) {
-     console.log(req.headers);
-     res.header({
-          "custom-header": "valor personalizado",
-          "cache-control": "max-age=60"
-     });
-     response.success(req, res, 'Lista de Mensajes!', 200);
+     controller.getMessages()
+          .then((messageList) => {
+               response.success(req, res, messageList, 200)
+          })
+          .catch(e => {
+               response.error(req, res, 'Unexpected Error', 500, e)
+          });
 });
 
 router.post('/', function (req, res) {
